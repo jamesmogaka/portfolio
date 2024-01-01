129a73c5c5880d206bc6b5631c0906c446f9b3fa
james mogaka <jamesoyondi23@gmail.com>
2024-01-01 13:24:45 +0300

    Modifications on the nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
 import { Instagram, Twitter, GitHub, Linkedin } from "react-feather";
 //
 //Define the infomation that the navigation needs to run
@@ -41,16 +39,22 @@ export const Navbar: React.FC<Iprops> = (props) => {
   //
   //Contains my logo
   const Logo = styled.img`
-    height: 50px;
+    height: 100%;
     width: 40px;
     border-radius: 20%;
   `;
   //
   //Holds the links to the various pages of my portfolio
-  const Navigation = styled.div``;
+  const Navigation = styled.div`
+    flex: 6;
+    display: flex;
+    justify-content: center;
+  `;
   //
   //Holds the links to various social platforms
-  const Contact = styled.div``;
+  const Contact = styled.div`
+    flex: 1;
+  `;
   //
   //Customised unorderd list to menu
   const List = styled.ul`
@@ -58,48 +62,21 @@ export const Navbar: React.FC<Iprops> = (props) => {
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
-
-    li {
-      margin: 0;
-      padding: 0;
-    }
-
-    /* Style the a tags within the ul */
-    a {
-      text-decoration: none;
-      color: #051320;
-      padding: 10px 15px;
-      position: relative;
-      transition: all 0.3s ease-in-out;
-
-      &::after {
-        content: "";
-        position: absolute;
-        width: 0;
-        height: 2px;
-        background-color: #c91c1c; /* Set the color of the line */
-        bottom: 0;
-        left: 0;
-        transition: width 0.3s ease-in-out;
-      }
-
-      &:hover {
-        &::after {
-          width: 100%;
-        }
-      }
-    }
   `;
   //
   //The navigation container styling
   const Container = styled.nav`
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    align-items: center;
     position: fixed;
-    top: 0;
     width: 100%;
-    height: 20%;
+    height: 5%;
+    gap: 20px;
+    background: rgba(240, 232, 255, 0.603);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
   `;
   //
   //Map the platform to its icon
@@ -119,7 +96,16 @@ export const Navbar: React.FC<Iprops> = (props) => {
       <Navigation>
         <NavigationMenu>
           <NavigationMenuList>
-            
+            {pages.map((page) => (
+              <NavigationMenuItem>
+                <Button variant="ghost">
+                  <NavigationMenuLink href={"#" + page}>
+                    {page}
+                  </NavigationMenuLink>
+                </Button>
+              </NavigationMenuItem>
+            ))}
+            <NavigationMenuIndicator></NavigationMenuIndicator>
           </NavigationMenuList>
         </NavigationMenu>
       </Navigation>
@@ -127,9 +113,11 @@ export const Navbar: React.FC<Iprops> = (props) => {
         <List>
           {contact.map((social, index) => (
             <li>
-              <a href={social.link} key={index}>
-                {platform_icons[social.platform]}
-              </a>
+              <Button variant="ghost" size="icon">
+                <a href={social.link} key={index} target="_blank">
+                  {platform_icons[social.platform]}
+                </a>
+              </Button>
             </li>
           ))}
         </List>
