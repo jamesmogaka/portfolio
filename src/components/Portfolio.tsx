6108bbccd4b94b044ab064ce177b2fa3068f0874
james mogaka <jamesoyondi23@gmail.com>
2024-01-02 17:16:13 +0300

    Addition of the portfolio tabs

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,16 +1,37 @@
-import React from "react";
-import styled from "styled-components";
+import styled from 'styled-components';
+import Android from './Android';
+import Web from './Web';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 //
 //Create a customized container
 const Section = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
-  padding-top: 50px;
+    height: 100vh;
+    width: 100%;
+    padding-top: 50px;
+    scroll-snap-align: center;
+    display: flex;
+    justify-content: center;
 `;
 //
 //Create a page to showcase my current and previous works
 const Portfolio = () => {
-  return <Section>Portfolio</Section>;
+    return (
+        <Section>
+            <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList>
+                    <TabsTrigger value="web">Web</TabsTrigger>
+                    <TabsTrigger value="android">Android</TabsTrigger>
+                </TabsList>
+                <TabsContent value="web">
+                    <Web />
+                </TabsContent>
+                <TabsContent value="android">
+                    <Android />
+                </TabsContent>
+            </Tabs>
+        </Section>
+    );
 };
 
 export default Portfolio;
