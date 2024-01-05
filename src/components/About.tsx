eaf00d4cec63bbd94860a02421dd531729fba832
james mogaka <jamesoyondi23@gmail.com>
2024-01-05 06:32:03 +0300

    seting the layout for the about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 //
 //Create a custom container
 const Section = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
-  padding-top: 50px;
+    height: 100vh;
+    scroll-snap-align: center;
+    padding-top: 50px;
 `;
 //
 //About-me page component
 const about = () => {
-  return <Section>about</Section>;
+    return (
+        <Section>
+            <h1>About me</h1>
+            <p></p>
+            <div></div>
+        </Section>
+    );
 };
 
 export default about;
