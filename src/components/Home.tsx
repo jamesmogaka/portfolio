23c79081990f5a90ebb0084f80e673183ab06ad1
james mogaka <jamesoyondi23@gmail.com>
2024-01-22 10:44:07 +0300

    Conversion to tailwind css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,18 +2,6 @@ import styled from 'styled-components';
 // import CV from "../assets/CV (1).pdf";
 import profile from '../assets/profile_bg.png';
 //
-//Create a css customized container
-const Section = styled.div`
-    height: 100%;
-    scroll-snap-align: center;
-    padding-top: 200px;
-    margin-left: 8%;
-    margin-top: 20%;
-    display: flex;
-    justify-content: space-between;
-    gap: 20px;
-`;
-//
 // Style the text conteiner
 const TextContainer = styled.div`
     flex: 2;
@@ -44,21 +32,6 @@ const TextContainer = styled.div`
         white-space: nowrap;
         overflow: hidden;
     }
-    button {
-        background-color: #007bff;
-        color: #fff;
-        padding: 10px 20px;
-        border-radius: 10px;
-        border: none;
-        margin-top: 10px;
-        margin-left: 25%;
-        align-self: center;
-        cursor: pointer;
-        a {
-            text-decoration: none;
-            color: white;
-        }
-    }
     button:hover {
         box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
             0 17px 50px 0 rgba(0, 0, 0, 0.19);
@@ -88,51 +61,36 @@ const TextContainer = styled.div`
     }
 `;
 //
-// Style the image component
-const ProfileImage = styled.div`
-    flex: 1;
-    text-align: right;
-    // Style the image
-    img {
-        max-width: 100%;
-        height: Auto;
-        margin-right: 10%;
-        border-radius: 50%;
-        transition: transform 0.3s ease;
-    }
-    img:hover {
-        transform: scale(1.1);
-        left: 45%;
-    }
-`;
-//
 //Create a home page for the entire portfolio
 const Home = () => {
     return (
         //
         // The home section that displays the home page
-        <Section id="home">
-            {/* The ProfileImage contains the image section in the home page */}
-            <ProfileImage>
-                <img src={profile} alt="My picture" />
-            </ProfileImage>
+        <div
+            id="home"
+            className=" h-dvh snap-center flex flex-col pt-[10%] gap-6 justify-center items-center sm:flex-row sm:justify-center sm:items-center sm:pt-[5%]"
+        >
+            <img
+                src={profile}
+                alt="My picture"
+                className=" sm:max-w-full max-w-[60%] h-[50%] object-cover rounded-[50%] transition-all duration-300 ease-in-out transform hover:scale-110 left-44 flex-grow-0"
+            />
 
-            {/* The Text Contaier contains the text part of the homepage */}
-            <TextContainer>
+            <div className=" flex-grow">
                 <h3>Hello,</h3>
                 <h1>
-                    I'm<span> &nbsp;James Mogaka.</span>
+                    I'm<span className=""> &nbsp;James Mogaka.</span>
                 </h1>
-                <h1>a digital craftsman.</h1>
-                <h1>a software engineer.</h1>
+                <h1 className="">a digital craftsman.</h1>
+                <h1 className="">a software engineer.</h1>
 
-                <button>
+                <button className=" bg-[#007bff] text-white p-2 px-4 border-none rounded-md cursor-pointer mt-2 ml-[25%]">
                     <a href="#" className="btn">
                         Resume
                     </a>
                 </button>
-            </TextContainer>
-        </Section>
+            </div>
+        </div>
     );
 };
 
