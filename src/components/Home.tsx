abb9c013fbf9a040c1e6a757a913ef6335db7de7
james mogaka <jamesoyondi23@gmail.com>
2024-01-23 23:31:06 +0300

    tailwind on home complete

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,65 +1,6 @@
-import styled from 'styled-components';
 // import CV from "../assets/CV (1).pdf";
 import profile from '../assets/profile_bg.png';
-//
-// Style the text conteiner
-const TextContainer = styled.div`
-    flex: 2;
-    margin-right: 20px;
-
-    // The css styling to my page
-    h1 {
-        font-size: 40px;
-        color: #333;
-        span {
-            color: blue;
-            animation: typing 10s infinite steps(20, end);
-            white-space: nowrap;
-            overflow: hidden;
-            display: inline-block;
-            position: relative;
-            vertical-align: middle;
-        }
-    }
-    //
-    //Indenting the h1 immedietly after a h1
-    h1 + h1 {
-        margin-left: 70px;
-        border-right: 4px solid #000;
-        margin-bottom: 2px;
-        animation: cursor 1s infinite step-end,
-            typing 10s infinite steps(20, end);
-        white-space: nowrap;
-        overflow: hidden;
-    }
-    button:hover {
-        box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
-            0 17px 50px 0 rgba(0, 0, 0, 0.19);
-    }
-    //Define the animations
-    @keyframes typing {
-        0%,
-        90%,
-        100% {
-            width: 0ch;
-        }
-        30%,
-        80% {
-            width: 17ch;
-        }
-    }
-
-    @keyframes cursor {
-        0%,
-        100% {
-            border-color: transparent;
-        }
-
-        50% {
-            border-color: #000;
-        }
-    }
-`;
+import '../index.css';
 //
 //Create a home page for the entire portfolio
 const Home = () => {
@@ -73,18 +14,25 @@ const Home = () => {
             <img
                 src={profile}
                 alt="My picture"
-                className=" sm:max-w-full max-w-[60%] h-[50%] object-cover rounded-[50%] transition-all duration-300 ease-in-out transform hover:scale-110 left-44 flex-grow-0"
+                className=" sm:max-w-full max-w-[60%] h-[50%] md:h-auto object-cover rounded-[50%] transition-all duration-300 ease-in-out transform hover:scale-110 left-44 "
             />
 
-            <div className=" flex-grow">
+            <div className=" flex-grow w-full ">
                 <h3>Hello,</h3>
-                <h1>
-                    I'm<span className=""> &nbsp;James Mogaka.</span>
+                <h1 className=" text-3xl">
+                    I'm
+                    <span className="typing text-blue-700 inline-block relative align-middle">
+                        &nbsp;James Mogaka.
+                    </span>
                 </h1>
-                <h1 className="">a digital craftsman.</h1>
-                <h1 className="">a software engineer.</h1>
+                <h1 className="cursor text-3xl ml-[70px] border-r-4 border-solid border-black mb-2">
+                    a digital craftsman.
+                </h1>
+                <h1 className="cursor  text-3xl ml-[70px] border-r-4 border-solid border-black mb-2">
+                    a software engineer.
+                </h1>
 
-                <button className=" bg-[#007bff] text-white p-2 px-4 border-none rounded-md cursor-pointer mt-2 ml-[25%]">
+                <button className=" bg-[#007bff] text-white p-2 px-4 border-none rounded-md cursor-pointer mt-2 ml-[40%]">
                     <a href="#" className="btn">
                         Resume
                     </a>
