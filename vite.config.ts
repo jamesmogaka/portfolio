92a1933e98fccd45fa7a4d05e7169d53f3ad88de
mogaka <jamesoyondi23@gmail.com>
2024-12-16 12:10:40 +0300

    setup the base correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      base: '/portfolio/', 
+    plugins: [react()],
+    base: '/portfolio/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
 });
