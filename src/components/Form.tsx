ac2035212e57f604765654552abdfa4cb0eb3b64
mogaka <jamesoyondi23@gmail.com>
2024-12-16 12:38:54 +0300

    clearing the form on successfull message

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -22,53 +22,47 @@ const Form: React.FC = () => {
         event.preventDefault();
         //
         // Actual submission of the email
-        emailjs
-            .sendForm(
-                'service_vb3p78l',
-                'template_rchgxzo',
-                form.current!,
-                'EszBrUayaWueS2UsX'
-            )
-            .then(
-                (result) => {
-                    //
-                    // Alert the user on the success
-                    toast({
-                        title: result.text,
-                        description: 'Message was sent successfully',
-                    });
-                    //
-                    // Reflect operation was successful
-                    set_success(!is_successfull);
-                },
-                (error) => {
-                    toast({
-                        style: { backgroundColor: 'green' },
-                        variant: 'destructive',
-                        title: 'Uh oh! Something went wrong.',
-                        description: error.message,
-                    });
-                }
-            );
+        emailjs.sendForm('service_vb3p78l', 'template_rchgxzo', form.current!, 'EszBrUayaWueS2UsX').then(
+            (result) => {
+                //
+                // Alert the user on the success
+                toast({
+                    title: result.text,
+                    description: 'Message was sent successfully',
+                });
+                //
+                // Reflect operation was successful
+                set_success(!is_successfull);
+                form.current?.reset();
+            },
+            (error) => {
+                toast({
+                    style: { backgroundColor: 'green' },
+                    variant: 'destructive',
+                    title: 'Uh oh! Something went wrong.',
+                    description: error.message,
+                });
+            },
+        );
     };
 
     return (
         <div className="flex justify-center items-center">
-            <form 
-                ref={form} 
+            <form
+                ref={form}
                 onSubmit={send_email}
                 className="w-[500px] flex flex-col justify-center items-center gap-[30px]"
             >
                 <h1 className="font-extralight p-0 m-0">Contact me</h1>
-                <input 
-                    type="text" 
-                    placeholder="Full Name" 
+                <input
+                    type="text"
+                    placeholder="Full Name"
                     name="name"
                     className="text-center w-[60%] p-5 border-none bg-[#d3d7d7] rounded-md"
                 />
-                <input 
-                    type="email" 
-                    placeholder="Email" 
+                <input
+                    type="email"
+                    placeholder="Email"
                     name="email"
                     className="text-center w-[60%] p-5 border-none bg-[#d3d7d7] rounded-md"
                 />
@@ -78,11 +72,7 @@ const Form: React.FC = () => {
                     name="message"
                     className="text-center w-[60%] p-5 h-20 border-none bg-[#d3d7d7] rounded-md"
                 ></textarea>
-                <button 
-                    className="h-[30px] w-[50%] self-center border-none rounded-[10px] bg-[#2986f0]"
-                >
-                    Send
-                </button>
+                <button className="h-[30px] w-[50%] self-center border-none rounded-[10px] bg-[#2986f0]">Send</button>
             </form>
         </div>
     );
